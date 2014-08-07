9bc60d84bbf1ed691b8ff5389a91e55a19d0e768
Cédric Belin <cedric@belin.io>
2014-08-07 14:29:24 +0200

    Added build system

---
+++ bin/make.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+/* global cd, config, echo, exec, target */
+
+/**
+ * Build system.
+ * @module bin/make
+ */
+'use strict';
+
+// Module dependencies.
+require('shelljs/make');
+
+/**
+ * Provides tasks for [ShellJS](http://shelljs.org) make tool.
+ * @class cli.Makefile
+ * @static
+ */
+cd(__dirname+'/..');
+
+/**
+ * The application settings.
+ * @property config
+ * @type Object
+ */
+config.fatal=true;
+
+/**
+ * Runs the default tasks.
+ * @method all
+ */
+target.all=function() {
+  echo('Please specify a target. Available targets:');
+  for(var task in target) {
+    if(task!='all') echo(' ', task);
+  }
+};
+
+/**
+ * Builds the documentation.
+ * @method doc
+ */
+target.doc=function() {
+  echo('Build the documentation...');
+  exec('yuidoc --no-code --no-color');
+};
+
+/**
+ * Performs static analysis of source code.
+ * @method lint
+ */
+target.lint=function() {
+  config.fatal=false;
+
+  echo('Static analysis of JavaScript sources...');
+  exec('jshint --verbose bin');
+
+  echo('Static analysis of documentation comments...');
+  exec('yuidoc --lint --no-color');
+
+  config.fatal=true;
+};
