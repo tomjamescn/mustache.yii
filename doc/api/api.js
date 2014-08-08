5809fb8fca19866548e2839aa9a5730bae22fedb
Cédric Belin <cedric@belin.io>
2014-08-08 23:57:03 +0200

    Added API reference

---
+++ doc/api/api.js
@@ -0,0 +1,38 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CMustacheCache",
+        "CMustacheLoader",
+        "CMustacheLogger",
+        "CMustacheRenderer"
+    ],
+    "modules": [
+        "CMustacheCache",
+        "CMustacheLoader",
+        "CMustacheLogger",
+        "CMustacheRenderer"
+    ],
+    "allModules": [
+        {
+            "displayName": "CMustacheCache",
+            "name": "CMustacheCache",
+            "description": "Implementation of the `CMustacheCache` class."
+        },
+        {
+            "displayName": "CMustacheLoader",
+            "name": "CMustacheLoader",
+            "description": "Implementation of the `CMustacheLoader` class."
+        },
+        {
+            "displayName": "CMustacheLogger",
+            "name": "CMustacheLogger",
+            "description": "Implementation of the `CMustacheLogger` class."
+        },
+        {
+            "displayName": "CMustacheRenderer",
+            "name": "CMustacheRenderer",
+            "description": "Implementation of the `CMustacheRenderer` class."
+        }
+    ]
+} };
+});
