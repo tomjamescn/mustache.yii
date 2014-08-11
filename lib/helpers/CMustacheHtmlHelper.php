b8168a345f2fdc664d3050f152c050155db39c9c
Cédric Belin <cedric@belin.io>
2014-08-11 17:35:24 +0200

    The class now extends from CMustacheHelper

--- lib/helpers/CMustacheHtmlHelper.php
+++ lib/helpers/CMustacheHtmlHelper.php
@@ -3,55 +3,53 @@
  * Implementation of the `CMustacheHtmlHelper` class.
  * @module CMustacheHtmlHelper
  */
+Yii::import('mustache.CMustacheHelper');
 
 /**
  * Component providing a collection of helper methods for creating views.
  * @class CMustacheHtmlHelper
- * @extends CComponent
+ * @extends CMustacheHelper
  * @constructor
  */
-class CMustacheHtmlHelper extends CComponent {
+class CMustacheHtmlHelper extends CMustacheHelper {
+
+  /**
+   * Creates an absolute URL for the specified route.
+	 * See: `CController->createAbsoluteUrl()`
+	 * @property absoluteUrl
+	 * @type Closure
+	 * @final
+	 */
+  public function getAbsoluteUrl() {
+    return function($value, Mustache_LambdaHelper $helper) {
+      $args=$this->parseArguments($helper->render($value), 'route', [
+        'ampersand'=>'&',
+        'params'=>[]
+      ]);
+
+      $controller=Yii::app()->controller;
+      $callback=($controller ? [ $controller, 'createAbsoluteUrl' ] : [ Yii::app(), 'createAbsoluteUrl' ]);
+      return call_user_func($callback, $args['route'], $args['params'], $args['ampersand']);
+    };
+  }
 
   /**
    * Generates the URL for the published assets.
+	 * See: `CHtml::asset()`
    * @property asset
    * @type Closure
    * @final
 	 */
 	public function getAsset() {
-    return function($path, Mustache_LambdaHelper $helper) {
-      return CHtml::asset($helper->render($path));
+    return function($value, Mustache_LambdaHelper $helper) {
+      $args=$this->parseArguments($helper->render($value), 'path', [ 'hashByName'=>false ]);
+      return CHtml::asset($args['path'], $args['hashByName']);
     };
 	}
 
   /**
-   * Creates an absolute URL for the specified route.
-	 * @property createAbsoluteUrl
-	 * @type Closure
-	 * @final
-	 */
-  public function getCreateAbsoluteUrl() {
-    return function($route, Mustache_LambdaHelper $helper) {
-      $value=$helper->render($route);
-      return ($controller=Yii::app()->controller) ? $controller->createAbsoluteUrl($value) : Yii::app()->createAbsoluteUrl($value);
-    };
-  }
-
-  /**
-   * Creates a relative URL for the specified route.
-   * @property createUrl
-   * @type Closure
-   * @final
-   */
-  public function getCreateUrl() {
-    return function($route, Mustache_LambdaHelper $helper) {
-      $value=$helper->render($route);
-      return ($controller=Yii::app()->controller) ? $controller->createUrl($value) : Yii::app()->createUrl($value);
-    };
-  }
-
-  /**
    * Generates a hidden field for storing the token used to perform CSRF validation.
+   * See: `CHttpRequest->csrfToken`
    * @property csrfTokenField
    * @type string
    * @final
@@ -62,43 +60,21 @@ class CMustacheHtmlHelper extends CComponent {
   }
 
   /**
-   * Formats a number using the decimal format defined in the locale.
-   * @property formatDecimal
-   * @type Closure
-   * @final
-   */
-  public function getFormatDecimal() {
-    return function($value, Mustache_LambdaHelper $helper) {
-      return Yii::app()->numberFormatter->formatDecimal($helper->render($value));
-    };
-  }
-
-  /**
-   * Formats a number using the percentage format defined in the locale.
-   * @property formatPercentage
-   * @type Closure
-   * @final
-	 */
-	public function getFormatPercentage($url, Mustache_LambdaHelper $helper) {
-    return function($value, Mustache_LambdaHelper $helper) {
-      return Yii::app()->numberFormatter->formatPercentage($helper->render($value));
-    };
-	}
-
-  /**
    * Generates a valid HTML identifier based on name.
+	 * See: `CHtml::idByName()`
    * @property idByName
    * @type Closure
    * @final
 	 */
   public function getIdByName() {
-    return function($name, Mustache_LambdaHelper $helper) {
-      return CHtml::idByName($helper->render($name));
+    return function($value, Mustache_LambdaHelper $helper) {
+      return CHtml::idByName($helper->render($value));
     };
   }
 
   /**
    * Generates a hidden field for storing persistent page states.
+	 * See: `CHtml::pageStateField()`
    * @property pageStateField
    * @type Closure
    * @final
@@ -110,17 +86,67 @@ class CMustacheHtmlHelper extends CComponent {
   }
 
   /**
-   * Registers a `refresh` meta tag.
+   * Returns a string that can be displayed on your Web page showing Powered-by-Yii information.
+	 * See: `Yii::powered()`
+   * @property powered
+   * @type string
+   * @final
+	 */
+  public function getPowered() {
+    return Yii::powered();
+  }
+
+  /**
+   * Registers a `refresh()` meta tag.
+	 * See: `CHtml::refresh`
    * @property refresh
    * @type Closure
    * @final
 	 */
 	public function getRefresh() {
     return function($value, Mustache_LambdaHelper $helper) {
-      $parts=explode(';', $helper->render($value), 2);
-      $seconds=$parts[0];
-      $url=(count($parts)>1 ? $parts[1] : '');
-      return CHtml::refresh($seconds, $url);
+      $args=$this->parseArguments($helper->render($value), 'seconds', [ 'url'=>'' ]);
+      return CHtml::refresh($args['seconds'], $args['url']);
     };
 	}
+
+  /**
+   * Translates a message to the specified language.
+	 * See: `Yii::t()`
+   * @property translate
+   * @type Closure
+   * @final
+   */
+  public function getTranslate() {
+    return function($value, Mustache_LambdaHelper $helper) {
+      $args=$this->parseArguments($helper->render($value), 'message', [
+        'category'=>'application',
+        'language'=>null,
+        'params'=>[],
+        'source'=>null
+      ]);
+
+      return CHtml::encode(Yii::t($args['category'], $args['message'], $args['params'], $args['source'], $args['language']));
+    };
+  }
+
+  /**
+   * Creates a relative URL for the specified route.
+	 * See: `CController->createUrl()`
+   * @property url
+   * @type Closure
+   * @final
+   */
+  public function getUrl() {
+    return function($value, Mustache_LambdaHelper $helper) {
+      $args=$this->parseArguments($helper->render($value), 'route', [
+        'ampersand'=>'&',
+        'params'=>[]
+      ]);
+
+      $controller=Yii::app()->controller;
+      $callback=($controller ? [ $controller, 'createUrl' ] : [ Yii::app(), 'createUrl' ]);
+      return call_user_func($callback, $args['route'], $args['params'], $args['ampersand']);
+    };
+  }
 }
