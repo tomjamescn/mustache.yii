691d1249abe3bed3506462221d5c505b58c606b2
Cédric Belin <cedric@belin.io>
2014-08-13 15:12:37 +0200

    Reserved for future use the "widget" helper

---
+++ lib/helpers/CMustacheWidgetHelper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Implementation of the `CMustacheWidgetHelper` class.
+ * @module CMustacheWidgetHelper
+ */
+Yii::import('mustache.CMustacheHelper');
+
+/**
+ * Provides a collection of helper methods for creating view widgets.
+ * @class CMustacheWidgetHelper
+ * @extends CMustacheHelper
+ * @constructor
+ */
+class CMustacheWidgetHelper extends CMustacheHelper {
+}
