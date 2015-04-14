324d7fc25d3c4a17e2f48ef999e2c394fd7a69f2
Cédric Belin <cedric@belin.io>
2015-04-14 08:29:38 +0200

    Added new URL helpers

--- lib/helpers/Url.php
+++ lib/helpers/Url.php
@@ -17,6 +17,48 @@ use yii\helpers\Url as UrlHelper;
 class Url extends Helper {
 
   /**
+   * Returns the base URL of the current request.
+   * See: `yii\helpers\Url::base()`
+   * @property base
+   * @type Closure
+   * @final
+   */
+  public function getBase() {
+    return function($value, \Mustache_LambdaHelper $helper) {
+      \Yii::trace($value);
+      // TODO
+      return UrlHelper::base($helper->render($value) ?: false);
+    };
+  }
+
+  /**
+   * Returns the canonical URL of the currently requested page.
+   * See: `yii\helpers\Url::canonical()`
+   * @property canonical
+   * @type string
+   * @final
+   */
+  public function getCanonical() {
+    return UrlHelper::canonical();
+  }
+
+  /**
+   * Returns the home URL.
+   * See: `yii\helpers\Url::current()`
+   * @property current
+   * @type Closure
+   * @final
+   */
+  public function getCurrent() {
+    return function($value, \Mustache_LambdaHelper $helper) {
+      \Yii::trace($value);
+      // TODO
+      $args=$this->parseArguments($helper->render($value), 'params', [ 'scheme'=>false ]);
+      return UrlHelper::current($args['params'], $args['scheme']);
+    };
+  }
+
+  /**
    * Returns the home URL.
    * See: `yii\helpers\Url::home()`
    * @property home
