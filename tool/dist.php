1844b8298a7b2e28eeb1c07e0e30addd20c0d90d
Cédric Belin <cedric@belin.io>
2015-07-19 23:23:36 +0200

    Fixed missing helpers

--- tool/dist.php
+++ tool/dist.php
@@ -11,4 +11,5 @@ $archive=new ZipArchive();
 $archive->open('var/mustache.yii-0.4.0.zip', ZipArchive::CREATE | ZipArchive::OVERWRITE);
 $archive->addGlob('*.{json,md,txt}', GLOB_BRACE);
 $archive->addGlob('lib/*.php');
+$archive->addGlob('lib/helpers/*.php');
 $archive->close();
