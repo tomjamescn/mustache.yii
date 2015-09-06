3a2e2d67d5930e43686ccc7330fb4cd0cf42c4a7
Cédric Belin <cedric@belin.io>
2015-09-06 15:24:19 +0200

    Added missing tests

--- test/helpers/FormatTest.php
+++ test/helpers/FormatTest.php
@@ -20,39 +20,83 @@ class FormatTest extends \PHPUnit_Framework_TestCase {
   private $helper;
 
   /**
-   * Tests the `currency` property.
+   * Tests the `getBoolean` method.
    */
-  public function testCurrency() {
+  public function testGetBoolean() {
+    $closure=(new Format())->getBoolean();
+    $this->assertEquals('No', $closure(false, $this->helper));
+    $this->assertEquals('No', $closure(0, $this->helper));
+    $this->assertEquals('Yes', $closure(true, $this->helper));
+    $this->assertEquals('Yes', $closure(1, $this->helper));
+  }
+
+  /**
+   * Tests the `getCurrency` method.
+   */
+  public function testGetCurrency() {
     $closure=(new Format())->getCurrency();
     $this->assertEquals('$100.00', $closure('100', $this->helper));
     $this->assertEquals('€1,234.56', $closure('{ "value": 1234.56, "currency": "EUR" }', $this->helper));
   }
 
   /**
-   * Tests the `decimal` property.
+   * Tests the `getDate` method.
    */
-  public function testDecimal() {
+  public function testGetDate() {
+    $closure=(new Format())->getDate();
+    $this->assertEquals('Jan 1, 2015', $closure('2015-01-01 12:00:00', $this->helper));
+  }
+
+  /**
+   * Tests the `getDateTime` method.
+   */
+  public function testGetDateTime() {
+    $closure=(new Format())->getDateTime();
+    $this->assertEquals('Jan 1, 2015, 1:00:00 PM', $closure('2015-01-01 12:00:00', $this->helper));
+  }
+
+  /**
+   * Tests the `getDecimal` method.
+   */
+  public function testGetDecimal() {
     $closure=(new Format())->getDecimal();
     $this->assertEquals('100.00', $closure('100', $this->helper));
     $this->assertEquals('1,234.56', $closure('1234.56', $this->helper));
   }
 
   /**
-   * Tests the `ntext` property.
+   * Tests the `getInteger` method.
    */
-  public function testNtext() {
+  public function testGetInteger() {
+    $closure=(new Format())->getInteger();
+    $this->assertEquals('100', $closure('100', $this->helper));
+    $this->assertEquals('-1,234', $closure('-1234.56', $this->helper));
+  }
+
+  /**
+   * Tests the `getNtext` method.
+   */
+  public function testGetNtext() {
     $closure=(new Format())->getNtext();
     $this->assertEquals('Foo<br>Bar', $closure("Foo\nBar", $this->helper));
     $this->assertEquals('Foo<br>Baz', $closure("Foo\r\nBaz", $this->helper));
   }
 
   /**
-   * Tests the `percent` property.
+   * Tests the `getPercent` method.
    */
-  public function testPercent() {
+  public function testGetPercent() {
     $closure=(new Format())->getPercent();
     $this->assertEquals('10%', $closure('0.1', $this->helper));
     $this->assertEquals('123%', $closure('1.23', $this->helper));
+  }
+
+  /**
+   * Tests the `getTime` method.
+   */
+  public function testGetTime() {
+    $closure=(new Format())->getTime();
+    $this->assertEquals('1:00:00 PM', $closure('2015-01-01 12:00:00', $this->helper));
   }
 
   /**
